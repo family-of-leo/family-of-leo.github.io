314af59026296771f60be8a050707bdefff29eea
chp320 <chp320@naver.com>
2025-06-01 21:12:50 +0900

    기능 수정

--- scripts/auth.js
+++ scripts/auth.js
@@ -4,11 +4,6 @@ const correctHash = "bcb072a82e2c420458dd48584a57f8cb7bbea821bfaed1c40aa802e542c
 function checkPassword() {
   const input = document.getElementById("password").value.trim();
 
-  // 디버깅용 로그
-  console.log("입력된 비밀번호:", input);
-  console.log("입력된 해시값:", sha256(input));
-  console.log("정답 해시값:", correctHash);
-
   const hash = sha256(input);
 
   if (hash === correctHash) {
